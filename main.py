25d78ef2ae1c47dab934d7b58514c99d7718479c
DanielWu <DanielWu@duck.com>
2022-11-16 17:15:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -310,7 +310,7 @@ urlSpeech = url_decode(
 # 关闭证书验证
 ssl._create_default_https_context = ssl._create_unverified_context
 
-with SB(uc=True) as sb:  # By default, browser="chrome" if not set.
+with SB(uc=True, extension_zip="uBlock_Origin_1_44_4_0.zip") as sb:  # By default, browser="chrome" if not set.
     print('- 🚀 loading...')
     if urlBase != '' and username != '' and password != '':
         try:
